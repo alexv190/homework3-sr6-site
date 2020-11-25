0dd408c81b74c175e603a673aef780ddaeacec99
Alexander <car3651@yandex.ru>
2020-11-25 23:41:53 +0500

    added 3 pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { MainComponent } from './pages/main/main.component';
+import { MyProjectsComponent } from './pages/my-projects/my-projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: MainComponent
+  },
+  {
+    path: 'about',
+    component: AboutMeComponent
+  },
+  {
+    path: 'projects',
+    component: MyProjectsComponent
+  }
+  ,
+  {
+    path: '404',
+    component: ErrorComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { MyProjectsComponent } from './pages/my-projects/my-projects.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { MainComponent } from './pages/main/main.component';
+import { HeaderComponent } from './pageparts/header/header.component';
+import { HistoryNavigationComponent } from './pageparts/history-navigation/history-navigation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    MyProjectsComponent,
+    ErrorComponent,
+    MainComponent,
+    HeaderComponent,
+    HistoryNavigationComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
